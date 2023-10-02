40d35898306fb8df9b070e657b86cf13f71bbc7d
Anmol <anmolsahu2504@gmail.com>
2023-10-02 19:35:55 +0530

    colorchanges

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { dark } from "@clerk/themes";
+
 
 import "../globals.css";
 
@@ -24,7 +24,7 @@ export default function RootLayout({
   return (
     <ClerkProvider
       appearance={{
-        baseTheme: dark,
+        
       }}
     >
       <html lang='en'>
